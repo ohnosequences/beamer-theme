6ce4f58a17b538ceb8fb880d48c7fe4d2f1a6af7
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-06-09 16:46:38 +0200

    remove unneeded bits

--- test-pandoc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -xe
-pandoc -t beamer --template=ohnosequences.beamer --latex-engine=xelatex pandoc-test.md -o pandoc-test.tex
-xelatex -shell-escape pandoc-test.tex
-xelatex -shell-escape pandoc-test.tex
